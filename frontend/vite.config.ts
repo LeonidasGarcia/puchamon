af64b260cfe3d3a41eb4f1c637ed51342501b8d5
leonidas garcia <leo.garcia.dev.ts@gmail.com>
2026-04-30 11:37:48 -0500

    Refactor frontend structure: remove App component, integrate Layout, and update styles
    - Deleted App.css and App.tsx files to streamline component structure.
    - Introduced Layout component to replace App, enhancing modularity.
    - Updated index.css to include new theme variables and font settings.
    - Modified main.tsx to render Layout instead of App.
    - Enhanced vite.config.ts with proxy settings for external API calls.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  server: {
+    proxy: {
+      "/poke-gifs": {
+        target: "https://img.pokemondb.net",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/poke-gifs/, ""),
+      },
+      "/mini-poke-sprites": {
+        target: "https://raw.githubusercontent.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/mini-poke-sprites/, ""),
+      },
+    },
+  },
+});
